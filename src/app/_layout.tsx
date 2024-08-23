6cc639394a958a3d6221712a5c010a7573ed5555
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-23 13:11:05 +0300

    initialization

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function layout() {
+	return (
+		<View>
+			<Text>layout</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({})
